c3bf3a0cb4ea889d142d69c89a3df231adbab045
tc.balaji@rever.com.sg <tc.balaji@rever.com.sg>
2021-01-19 17:21:45 +0530

    Add project files.

---
+++ Form2_S2.vb
@@ -0,0 +1,313 @@
+Public Class Form2_S2
+    Inherits System.Windows.Forms.Form
+
+    Public Declare Function GetSystemMenu Lib "user32.dll" Alias "GetSystemMenu" (ByVal hwnd As IntPtr, ByVal bRevert As Long) As IntPtr
+    Public Declare Function RemoveMenu Lib "user32.dll" Alias "RemoveMenu" (ByVal hMenu As IntPtr, ByVal nPosition As Long, ByVal wFlags As Long) As Long
+    Public Const SC_CLOSE As Long = &HF060
+    Public Const MF_BYCOMMAND As Long = &H0
+
+    Dim SqlCmd1 As SqlClient.SqlCommand
+    Dim DaList1 = New SqlClient.SqlDataAdapter
+    Dim DsList1 As New DataSet
+    Dim DtView1 As DataView
+
+    Dim strSQL As String
+
+#Region " Windows フォーム デザイナで生成されたコード "
+
+    Public Sub New()
+        MyBase.New()
+
+        ' この呼び出しは Windows フォーム デザイナで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後に初期化を追加します。
+
+    End Sub
+
+    ' Form は、コンポーネント一覧に後処理を実行するために dispose をオーバーライドします。
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    ' Windows フォーム デザイナで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    ' メモ : 以下のプロシージャは、Windows フォーム デザイナで必要です。
+    'Windows フォーム デザイナを使って変更してください。  
+    ' コード エディタを使って変更しないでください。
+    Friend WithEvents DataGridTableStyle1 As System.Windows.Forms.DataGridTableStyle
+    Friend WithEvents DataGridTextBoxColumn1 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn2 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn3 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn4 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridEx1 As MrMAX_Wrn.DataGridEx
+    Friend WithEvents DataGridTextBoxColumn9 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn10 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn11 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn5 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn6 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn7 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents DataGridTextBoxColumn8 As System.Windows.Forms.DataGridTextBoxColumn
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Me.DataGridTableStyle1 = New System.Windows.Forms.DataGridTableStyle
+        Me.DataGridEx1 = New MrMAX_Wrn.DataGridEx
+        Me.DataGridTextBoxColumn9 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn10 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn11 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn5 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn6 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn7 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn8 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn1 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn2 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn3 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.DataGridTextBoxColumn4 = New System.Windows.Forms.DataGridTextBoxColumn
+        Me.Button11 = New System.Windows.Forms.Button
+        CType(Me.DataGridEx1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridTableStyle1
+        '
+        Me.DataGridTableStyle1.AlternatingBackColor = System.Drawing.Color.FromArgb(CType(192, Byte), CType(192, Byte), CType(255, Byte))
+        Me.DataGridTableStyle1.DataGrid = Me.DataGridEx1
+        Me.DataGridTableStyle1.GridColumnStyles.AddRange(New System.Windows.Forms.DataGridColumnStyle() {Me.DataGridTextBoxColumn9, Me.DataGridTextBoxColumn10, Me.DataGridTextBoxColumn11, Me.DataGridTextBoxColumn5, Me.DataGridTextBoxColumn6, Me.DataGridTextBoxColumn7, Me.DataGridTextBoxColumn8, Me.DataGridTextBoxColumn1, Me.DataGridTextBoxColumn2, Me.DataGridTextBoxColumn3, Me.DataGridTextBoxColumn4})
+        Me.DataGridTableStyle1.HeaderForeColor = System.Drawing.SystemColors.ControlText
+        Me.DataGridTableStyle1.MappingName = "WRN_DATA"
+        Me.DataGridTableStyle1.ReadOnly = True
+        '
+        'DataGridEx1
+        '
+        Me.DataGridEx1.AlternatingBackColor = System.Drawing.Color.FromArgb(CType(192, Byte), CType(192, Byte), CType(255, Byte))
+        Me.DataGridEx1.CaptionVisible = False
+        Me.DataGridEx1.DataMember = ""
+        Me.DataGridEx1.Font = New System.Drawing.Font("MS UI Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.DataGridEx1.HeaderForeColor = System.Drawing.SystemColors.ControlText
+        Me.DataGridEx1.Location = New System.Drawing.Point(8, 16)
+        Me.DataGridEx1.Name = "DataGridEx1"
+        Me.DataGridEx1.ReadOnly = True
+        Me.DataGridEx1.RowHeaderWidth = 0
+        Me.DataGridEx1.Size = New System.Drawing.Size(976, 492)
+        Me.DataGridEx1.TabIndex = 151
+        Me.DataGridEx1.TableStyles.AddRange(New System.Windows.Forms.DataGridTableStyle() {Me.DataGridTableStyle1})
+        '
+        'DataGridTextBoxColumn9
+        '
+        Me.DataGridTextBoxColumn9.Alignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.DataGridTextBoxColumn9.Format = ""
+        Me.DataGridTextBoxColumn9.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn9.HeaderText = "保証番号"
+        Me.DataGridTextBoxColumn9.MappingName = "WRN_NO"
+        Me.DataGridTextBoxColumn9.Width = 130
+        '
+        'DataGridTextBoxColumn10
+        '
+        Me.DataGridTextBoxColumn10.Alignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.DataGridTextBoxColumn10.Format = ""
+        Me.DataGridTextBoxColumn10.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn10.HeaderText = "状況"
+        Me.DataGridTextBoxColumn10.MappingName = "STS_NAME"
+        Me.DataGridTextBoxColumn10.Width = 25
+        '
+        'DataGridTextBoxColumn11
+        '
+        Me.DataGridTextBoxColumn11.Alignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.DataGridTextBoxColumn11.Format = ""
+        Me.DataGridTextBoxColumn11.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn11.HeaderText = "受付番号"
+        Me.DataGridTextBoxColumn11.MappingName = "ICDT_NO"
+        Me.DataGridTextBoxColumn11.NullText = ""
+        Me.DataGridTextBoxColumn11.ReadOnly = True
+        Me.DataGridTextBoxColumn11.Width = 60
+        '
+        'DataGridTextBoxColumn5
+        '
+        Me.DataGridTextBoxColumn5.Format = ""
+        Me.DataGridTextBoxColumn5.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn5.HeaderText = "氏名"
+        Me.DataGridTextBoxColumn5.MappingName = "CUST_NAME"
+        Me.DataGridTextBoxColumn5.ReadOnly = True
+        Me.DataGridTextBoxColumn5.Width = 105
+        '
+        'DataGridTextBoxColumn6
+        '
+        Me.DataGridTextBoxColumn6.Alignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.DataGridTextBoxColumn6.Format = ""
+        Me.DataGridTextBoxColumn6.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn6.HeaderText = "電話番号"
+        Me.DataGridTextBoxColumn6.MappingName = "TEL_NO"
+        Me.DataGridTextBoxColumn6.ReadOnly = True
+        Me.DataGridTextBoxColumn6.Width = 85
+        '
+        'DataGridTextBoxColumn7
+        '
+        Me.DataGridTextBoxColumn7.Format = ""
+        Me.DataGridTextBoxColumn7.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn7.HeaderText = "品　名"
+        Me.DataGridTextBoxColumn7.MappingName = "MKR_NAME"
+        Me.DataGridTextBoxColumn7.ReadOnly = True
+        Me.DataGridTextBoxColumn7.Width = 155
+        '
+        'DataGridTextBoxColumn8
+        '
+        Me.DataGridTextBoxColumn8.Format = ""
+        Me.DataGridTextBoxColumn8.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn8.HeaderText = "部　門"
+        Me.DataGridTextBoxColumn8.MappingName = "CAT_NAME"
+        Me.DataGridTextBoxColumn8.ReadOnly = True
+        Me.DataGridTextBoxColumn8.Width = 60
+        '
+        'DataGridTextBoxColumn1
+        '
+        Me.DataGridTextBoxColumn1.Format = ""
+        Me.DataGridTextBoxColumn1.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn1.HeaderText = "型　式"
+        Me.DataGridTextBoxColumn1.MappingName = "MODEL"
+        Me.DataGridTextBoxColumn1.ReadOnly = True
+        Me.DataGridTextBoxColumn1.Width = 120
+        '
+        'DataGridTextBoxColumn2
+        '
+        Me.DataGridTextBoxColumn2.Alignment = System.Windows.Forms.HorizontalAlignment.Center
+        Me.DataGridTextBoxColumn2.Format = ""
+        Me.DataGridTextBoxColumn2.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn2.HeaderText = "加入日"
+        Me.DataGridTextBoxColumn2.MappingName = "WRN_DATE"
+        Me.DataGridTextBoxColumn2.ReadOnly = True
+        Me.DataGridTextBoxColumn2.Width = 80
+        '
+        'DataGridTextBoxColumn3
+        '
+        Me.DataGridTextBoxColumn3.Alignment = System.Windows.Forms.HorizontalAlignment.Right
+        Me.DataGridTextBoxColumn3.Format = "##,##0"
+        Me.DataGridTextBoxColumn3.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn3.HeaderText = "購入金額"
+        Me.DataGridTextBoxColumn3.MappingName = "PRICE"
+        Me.DataGridTextBoxColumn3.ReadOnly = True
+        Me.DataGridTextBoxColumn3.Width = 65
+        '
+        'DataGridTextBoxColumn4
+        '
+        Me.DataGridTextBoxColumn4.Alignment = System.Windows.Forms.HorizontalAlignment.Right
+        Me.DataGridTextBoxColumn4.Format = "##,##0"
+        Me.DataGridTextBoxColumn4.FormatInfo = Nothing
+        Me.DataGridTextBoxColumn4.HeaderText = "保証料"
+        Me.DataGridTextBoxColumn4.MappingName = "WRN_PRICE"
+        Me.DataGridTextBoxColumn4.ReadOnly = True
+        Me.DataGridTextBoxColumn4.Width = 55
+        '
+        'Button11
+        '
+        Me.Button11.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button11.Font = New System.Drawing.Font("Arial", 12.0!)
+        Me.Button11.Location = New System.Drawing.Point(832, 512)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(96, 30)
+        Me.Button11.TabIndex = 150
+        Me.Button11.TabStop = False
+        Me.Button11.Text = "閉じる"
+        '
+        'Form2_S2
+        '
+        Me.AutoScaleBaseSize = New System.Drawing.Size(7, 15)
+        Me.ClientSize = New System.Drawing.Size(990, 547)
+        Me.Controls.Add(Me.DataGridEx1)
+        Me.Controls.Add(Me.Button11)
+        Me.Font = New System.Drawing.Font("MS UI Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MinimizeBox = False
+        Me.Name = "Form2_S2"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "選択"
+        CType(Me.DataGridEx1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+#End Region
+    '*************************************************
+    '** 起動時
+    '*************************************************
+    Private Sub Form2_S2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        '×閉じるを使用不可
+        Dim lngH As IntPtr
+        lngH = GetSystemMenu(Me.Handle, 0)
+        RemoveMenu(lngH, SC_CLOSE, MF_BYCOMMAND)
+
+        DataGridEx1.DataSource = DtTbl0
+    End Sub
+
+    '********************************************************************
+    '**  データグリッド色
+    '********************************************************************
+    Private Sub DataGridEx1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles DataGridEx1.Paint
+        If DataGridEx1.CurrentRowIndex >= 0 Then
+            DataGridEx1.Select(DataGridEx1.CurrentRowIndex)
+        End If
+    End Sub
+
+    '********************************************************************
+    '**  データグリッド　クリック
+    '********************************************************************
+    Private Sub DataGridEx1_MouseUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles DataGridEx1.MouseUp
+        Dim hitinfo As DataGrid.HitTestInfo
+
+        Try
+            hitinfo = (CType(sender, DataGrid).HitTest(e.X, e.Y))
+            If hitinfo.Row >= 0 Then
+                Me.Cursor.Current = Cursors.WaitCursor
+                pWrn_no = DataGridEx1(DataGridEx1.CurrentRowIndex, 0)
+                pIcdt_no = DataGridEx1(DataGridEx1.CurrentRowIndex, 2)
+                pMode = "r"
+
+                'DsList1.Clear()
+                'strSQL = "SELECT ICDT_DATA.ICDT_NO, ICDT_DATA.FIN_FLAG"
+                'strSQL = strSQL & " FROM ICDT_DATA RIGHT OUTER JOIN WRN_DATA ON ICDT_DATA.WRN_NO = WRN_DATA.WRN_NO"
+                'strSQL = strSQL & " WHERE WRN_DATA.WRN_NO = '" & pWrn_no & "'"
+                'SqlCmd1 = New SqlClient.SqlCommand(strSQL, cnsqlclient)
+                'DaList1.SelectCommand = SqlCmd1
+                'DB_OPEN()
+                'DaList1.Fill(DsList1, "SELECT_DATA")
+                'DB_CLOSE()
+
+                'DtView1 = New DataView(DsList1.Tables("SELECT_DATA"), "FIN_FLAG <> '1'", "", DataViewRowState.CurrentRows)
+                'If DtView1.Count = 1 Then
+                '    MsgBox("現在、この保証番号には未完了のインシデントがあります。", MsgBoxStyle.OKOnly, "Warranty System")
+                '    pMode = "r"
+                '    pIcdt_no = DtView1(0)("ICDT_NO")
+                'Else
+                '    DtView1 = New DataView(DsList1.Tables("SELECT_DATA"), "FIN_FLAG = '1'", "", DataViewRowState.CurrentRows)
+                '    If DtView1.Count = 1 Then
+                '        pMode = "r"
+                '        pIcdt_no = DtView1(0)("ICDT_NO")
+                '    Else
+                '        pMode = "n"
+                '    End If
+                'End If
+
+                Dim frmform3 As New Form3
+                frmform3.ShowDialog()
+
+                Me.Cursor.Current = Cursors.Default
+            End If
+        Catch ex As System.Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '*************************************************
+    '**  戻る
+    '*************************************************
+    Private Sub Button11_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button11.Click
+        DsList1.Clear()
+        Me.Close()
+    End Sub
+End Class
