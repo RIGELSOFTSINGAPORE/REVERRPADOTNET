9fcc3f035c74fe10dc919c6e97cfbfe7ad00f386
Ramsanthosh <ramsanthosh.g@rever.com.sg>
2022-02-15 10:11:06 +0530

    KAisokku

---
+++ Kaisokku_Upload_Download/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kaisokku_Upload_Download.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        private string _ConnectionString = ConfigurationManager.ConnectionStrings["Kaisokku_SaleDBContext"].ConnectionString;
+        public string ConnectionString
+        {
+            get
+            {
+                return _ConnectionString;
+            }
+        }
+
+        public BaseController()
+        {
+
+        }
+    }
+}
---
+++ Kaisokku_Upload_Download/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kaisokku_Upload_Download.MvcApplication" Language="C#" %>
